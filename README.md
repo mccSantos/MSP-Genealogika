321c49a68b0ea04edf9fb2a4f950b72ee945e21e
Miguel Santos <miguel.c.csantos@sapo.pt>
2022-05-11 03:24:05 +0100

    registo que não consegue mandar posts

--- README.md
+++
@@ -1,4 +0,0 @@
-# MSP-Genealogika
-
-Functional prototype for Modelation of Systems and Process course.
-Group of 4.
---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client'
+
+const app = express();
+app.use(express.json);
+
+const prisma = new PrismaClient();
+
+app.post("/register", async(req: Request, res: Response) => {
+    const {name, email, password} = req.body;
+    const user = await prisma.user.create({
+        data: {
+            name: name,
+            email: email,
+            password: password
+        },
+    });
+    res.json(user);
+});
+
+app.listen(4000, () => console.log('Server is running on port 4000.'));
