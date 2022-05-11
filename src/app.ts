50d81d90df409dffd6529f3d18b61727482708e7
Miguel Santos <miguel.c.csantos@sapo.pt>
2022-05-11 11:21:58 +0100

    Register Works

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client'
 
 const app = express();
-app.use(express.json);
+app.use(express.json());
 
 const prisma = new PrismaClient();
 
@@ -17,5 +17,14 @@ app.post("/register", async(req: Request, res: Response) => {
     });
     res.json(user);
 });
+
+
+app.post("/login", async (req: Request, res: Response) => {
+    const {email, password} = req.body;
+    
+})
+app.get("/", async (req, res) => {
+    res.send("ola");
+})
 
 app.listen(4000, () => console.log('Server is running on port 4000.'));
