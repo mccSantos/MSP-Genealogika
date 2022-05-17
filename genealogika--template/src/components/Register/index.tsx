190513ff4ad845ccaacc3b8a4f60c719a90df3d9
Miguel Santos <miguel.c.csantos@sapo.pt>
2022-05-17 23:23:14 +0100

    register webpage

--- genealogika--template/src/components/Register/index.tsx
+++ genealogika--template/src/components/Register/index.tsx
@@ -4,29 +4,48 @@ import Button from "react-bootstrap/button";
 
 export function Register() {
   return (
-    <div>
-      <img src={logo} alt="Genealogika" />
-      <div>
-        <form className={styles.container}>
-          <div className={styles.label}>
-            Email
-            <br />
-            <input type="text" id="email" name="email" />
-          </div>
-          <br></br>
-          <div className={[styles.label, styles.passwordlabel].join(" ")}>
-            Password
-            <br />
-            <input type="password" id="password" name="password" />
-          </div>
-          <Button
-            type="submit"
-            variant="success"
-            className={styles.buttonsubmit}
-          >
-            Login
-          </Button>
-        </form>
+    <div className={styles.background}>
+      <div className={styles.formdiv}>
+        <img src={logo} alt="Genealogika" />
+        <div>
+          <form className={styles.container}>
+            <div className={styles.label}>
+              Name
+              <br />
+              <input type="text" id="name" name="name" />
+            </div>
+            <div className={[styles.label, styles.passwordlabel].join(" ")}>
+              Email
+              <br />
+              <input type="text" id="email" name="email" />
+            </div>
+            <br></br>
+            <div className={[styles.label, styles.passwordlabel].join(" ")}>
+              Password
+              <br />
+              <input type="password" id="password" name="password" />
+            </div>
+            <br></br>
+            <div className={[styles.label, styles.passwordlabel].join(" ")}>
+              Password Confirmation
+              <br />
+              <input
+                type="password"
+                id="passwordConfirmation"
+                name="password"
+              />
+            </div>
+            <div className={styles.buttonGroup}>
+              <Button
+                type="submit"
+                variant="success"
+                className={styles.buttonsubmit}
+              >
+                Register
+              </Button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
