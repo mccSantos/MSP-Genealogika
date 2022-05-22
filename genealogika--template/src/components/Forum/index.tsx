6af730c4b6f772f0de96eab24f5d781112ac5a87
Miguel Santos <miguel.c.csantos@sapo.pt>
2022-05-22 23:12:43 +0100

    button not Button

--- genealogika--template/src/components/Forum/index.tsx
+++ genealogika--template/src/components/Forum/index.tsx
@@ -1,4 +1,3 @@
-
 import styles from "./styles.module.scss";
 import logo from "../../assets/Genealogika_logo.png";
 import { useNavigate } from "react-router-dom";
@@ -8,37 +7,32 @@ import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
-import Button from "react-bootstrap/Button";
+import Button from "react-bootstrap/button";
 import { useState, FormEvent } from "react";
 
+export function Forum() {
+  const [topic, setTopic] = useState("");
 
+  async function handleSearch(event: FormEvent) {
+    if (!topic.trim()) return;
+  }
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="/">Home</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
 
-export function Forum(){
-    const [topic, setTopic] = useState("");
-    
-    async function handleSearch(event: FormEvent){
-        if(!topic.trim())
-            return;
-
-    }
-    return(
-        <Navbar bg="light" expand="lg">
-            <Container fluid>
-                <Navbar.Brand href="/">Home</Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll" />
-
-                <Form className="d-flex">
-                    <FormControl
-                        type="search"
-                        placeholder="Search"
-                        className="me-2"
-                        aria-label="Search"
-                        onSubmit =  {handleSearch}
-                        />
-                    <Button variant="outline-success" >Search</Button>
-                </Form>
-            </Container>
-        </Navbar>
-
-    );
-}   
+        <Form className="d-flex">
+          <FormControl
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+            onSubmit={handleSearch}
+          />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Container>
+    </Navbar>
+  );
+}
--- genealogika--template/src/components/Home/index.tsx
+++ genealogika--template/src/components/Home/index.tsx
@@ -1,24 +1,21 @@
-
 import styles from "./styles.module.scss";
 import logo from "../../assets/Genealogika_logo.png";
 import Button from "react-bootstrap/button";
 import { useNavigate } from "react-router-dom";
 
+export function Home() {
+  const navigate = useNavigate();
 
-export function Home(){
-    const navigate = useNavigate();
-    
-
-    return(
-            <div className={styles.navbar}>
-            
-            <a href="/" ><img src={logo} alt="Genealogika" /></a>
-            <a href="Forum">Forum</a>
-            <a href="Help">Help</a>
-            <a href="Login" className="right">Login</a>
-            </div>
-  
-        
-
-    );
+  return (
+    <div className={styles.navbar}>
+      <a href="/">
+        <img src={logo} alt="Genealogika" />
+      </a>
+      <a href="Forum">Forum</a>
+      <a href="Help">Help</a>
+      <a href="Login" className="right">
+        Login
+      </a>
+    </div>
+  );
 }
